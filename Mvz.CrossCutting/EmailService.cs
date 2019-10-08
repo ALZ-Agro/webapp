22f99d3775037bf2af36136e1b7aa7e189410524
ALZ-Agro <55987993+ALZ-Agro@users.noreply.github.com>
2019-10-08 10:53:48 -0300

    Primer commit

---
+++ Mvz.CrossCutting/EmailService.cs
@@ -0,0 +1,117 @@
+﻿using ALZAGRO.AppRendicionGastos.Fwk.CrossCutting;
+using System;
+using System.Configuration;
+
+namespace ALZAGRO.AppRendicionGastos.CrossCutting {
+    public class EmailService : IEmailService {
+
+        public void SendGenericMail(string subject, string message) {
+            try {
+                SendGenericMail("", subject, message);
+            }
+            catch (Exception) {
+
+            }
+        }
+
+        public void SendGenericMail(string to, string subject, string message) {
+            try {
+                String useRelay = ConfigurationManager.AppSettings["UseRelay"].ToString();
+
+                if (useRelay == "true") {
+                    //SendByRelay(subject, message, to);
+                }
+                else {
+                    SendNormalMail(to, subject, message);
+                }
+            }
+            catch (Exception) {
+
+            }
+        }
+
+        //private void SendByRelay(string subject, string message, String to) {
+        //    String relaySenderMail = ConfigurationManager.AppSettings["RelaySenderMail"].ToString();
+        //    String relaySenderText = ConfigurationManager.AppSettings["RelaySenderText"].ToString();
+
+
+        //    System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+        //    mail.From = new System.Net.Mail.MailAddress(relaySenderMail, relaySenderText);
+        //    mail.ReplyTo = new System.Net.Mail.MailAddress(relaySenderMail, relaySenderText);
+
+        //    if (!String.IsNullOrEmpty(to)) {
+        //        mail.To.Add(to);
+        //    }
+        //    else {
+
+        //        var mailReceivers = ConfigurationManager.AppSettings["MailReceiver"].ToString().Split(';');
+        //        foreach (var item in mailReceivers) {
+        //            if (!String.IsNullOrEmpty(item)) {
+        //                mail.To.Add(item);
+        //            }
+        //        }
+        //    }
+
+        //    mail.Subject = subject;
+        //    mail.Body = message;
+        //    mail.IsBodyHtml = true;
+        //    mail.Priority = System.Net.Mail.MailPriority.Normal;
+        //    System.Net.Mail.SmtpClient Smtp = new System.Net.Mail.SmtpClient();
+
+        //    String relayHost = ConfigurationManager.AppSettings["RelayHost"].ToString();
+
+        //    Smtp.Host = relayHost;
+        //    Smtp.Send(mail);
+        //}
+
+        private void SendNormalMail(String to, string subject, string message) {
+
+            System.Net.Mail.MailMessage mailMessage = new System.Net.Mail.MailMessage();
+
+            String sendMail = ConfigurationManager.AppSettings["SendMail"].ToString();
+            if (sendMail == "true") {
+                String mailSender = ConfigurationManager.AppSettings["MailSender"].ToString();
+                var mailReceivers = ConfigurationManager.AppSettings["MailReceiver"].ToString().Split(';');
+
+                String host = ConfigurationManager.AppSettings["Host"].ToString();
+                String passwordMailSender = ConfigurationManager.AppSettings["PasswordMailSender"].ToString();
+                String useDefaultCredentials = ConfigurationManager.AppSettings["UseDefaultCredentials"].ToString();
+
+                if (mailSender != null && mailReceivers != null && host != null) {
+                    mailMessage.From = new System.Net.Mail.MailAddress(mailSender);
+
+                    if (!String.IsNullOrEmpty(to)) {
+                        mailMessage.To.Add(to);
+                    }
+                    else {
+                        foreach (var item in mailReceivers) {
+                            if (!String.IsNullOrEmpty(item)) {
+                                mailMessage.To.Add(item);
+                            }
+                        }
+                    }
+
+                    mailMessage.Subject = subject;
+                    mailMessage.Body = message;
+                    mailMessage.IsBodyHtml = true;
+                    System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient();
+                    smtp.EnableSsl = true;
+                    smtp.Host = host;
+
+                    if (!String.IsNullOrWhiteSpace(passwordMailSender)) {
+                        smtp.Credentials = new System.Net.NetworkCredential(mailSender, passwordMailSender);
+                    }
+                    else {
+                        smtp.UseDefaultCredentials = Convert.ToBoolean(useDefaultCredentials);
+                    }
+                    try {
+                        smtp.Send(mailMessage);
+                    }
+                    catch (Exception ex) {
+                        System.Diagnostics.Trace.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Mvz.CrossCutting/MembershipService.cs
@@ -0,0 +1,161 @@
+﻿using ALZAGRO.AppRendicionGastos.Domain.Entities;
+using ALZAGRO.AppRendicionGastos.Fwk.CrossCutting;
+using ALZAGRO.AppRendicionGastos.Fwk.Culture;
+using ALZAGRO.AppRendicionGastos.Fwk.Domain;
+using ALZAGRO.AppRendicionGastos.Fwk.Domain.Entities;
+using System;
+using System.Linq;
+using System.Security.Principal;
+
+namespace ALZAGRO.AppRendicionGastos.CrossCutting {
+
+    public class MembershipService : IMembershipService
+    {
+
+        #region Variables
+
+        private readonly IEntityBaseRepository<User> userRepository;
+        private readonly IEncryptionService encryptionService;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly ITimeService timeService;
+
+        #endregion
+
+        public MembershipService(IEntityBaseRepository<User> userRepository,
+                                 IEncryptionService encryptionService,
+                                 ITimeService timeService,
+                                 IUnitOfWork unitOfWork)
+        {
+            this.userRepository = userRepository;
+            this.encryptionService = encryptionService;
+            this.unitOfWork = unitOfWork;
+            this.timeService = timeService;
+        }
+
+        #region IMembershipService Implementation
+
+        public MembershipContext ValidateUser(String username, String password)
+        {
+            var membershipCtx = new MembershipContext();
+
+            var user = userRepository.GetAll().Where(x => x.Email == username || x.Username == username).FirstOrDefault();
+
+            if (user != null && IsUserValid(user, password))
+            {
+                membershipCtx.User = user;
+                //var identity = new GenericIdentity(user.Username);
+                membershipCtx.Principal = new CustomPrincipal(user.Username, user.Id, user.FirstName, user.LastName);
+            }
+
+            return membershipCtx;
+        }
+
+        public IUser UpdateUser(String username, String email, String password)
+        {
+
+            var existingUser = userRepository.GetAll().Where(x => x.Username == username).FirstOrDefault();
+
+            if (existingUser == null)
+            {
+                throw new Exception(String.Format("El usuario {0} no está registrado.", username));
+            }
+
+            var passwordSalt = encryptionService.CreateSalt();
+
+            existingUser.Salt = passwordSalt;
+            existingUser.Email = email;
+            existingUser.HashedPassword = encryptionService.EncryptPassword(password, passwordSalt);
+
+            unitOfWork.Commit();
+
+            return existingUser;
+        }
+
+        public IUser UpdateUser(String username, String email)
+        {
+
+            var existingUser = userRepository.GetAll().Where(x => x.Username == username).FirstOrDefault();
+
+            if (existingUser == null)
+            {
+                throw new Exception(String.Format("El usuario {0} no est&aacute; registrado.", username));
+            }
+
+            existingUser.Email = email;
+
+            unitOfWork.Commit();
+
+            return existingUser;
+        }
+        public IUser CreateUser(String username, String email, String password, Int64[] roles)
+        {
+            var existingUser = userRepository.GetAll().Where(x => x.Username == username).FirstOrDefault();
+
+            if (existingUser != null)
+            {
+                throw new Exception(String.Format("El nombre de usuario {0} ya est&aacute; asignado.", username));
+            }
+
+            var passwordSalt = encryptionService.CreateSalt();
+
+            var user = new User()
+            {
+                Username = username,
+                Salt = passwordSalt,
+                Email = email,
+                HashedPassword = encryptionService.EncryptPassword(password, passwordSalt)
+                //CreatedDate = timeService.DateTimeNow
+            };
+
+            userRepository.Add(user);
+
+            unitOfWork.Commit();
+
+            return user;
+        }
+
+        public IUser GetUser(Int64 userId)
+        {
+            return userRepository.GetSingle(userId);
+        }
+
+        public Boolean UserExist(Int64 userId, String username)
+        {
+            var existingUser = userRepository.GetAll().Where(x => x.Username == username && x.Id != userId).FirstOrDefault();
+            return (existingUser != null);
+        }
+
+        public Boolean UserEmailExist(Int64 userId, String email)
+        {
+            var existingUser = userRepository.GetAll().Where(x => x.Email == email && x.Id != userId).FirstOrDefault();
+            return (existingUser != null);
+        }
+
+        public String CreateRandomPassword()
+        {
+            String randomPassword = Guid.NewGuid().ToString().Substring(0, 8);
+            return randomPassword;
+        }
+
+        #endregion
+
+        #region Helper methods
+
+        private bool IsPasswordValid(User user, String password)
+        {
+            return String.Equals(encryptionService.EncryptPassword(password, user.Salt), user.HashedPassword);
+        }
+
+        private bool IsUserValid(User user, String password)
+        {
+            if (IsPasswordValid(user, password))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
